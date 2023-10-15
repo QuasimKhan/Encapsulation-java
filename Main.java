1a03d25927c2297151c43dbc2bed004ba7f62e39
QuasimKhan <quasimkhanpersonal@gmail.com>
2023-10-15 17:28:01 +0530

    Updated

--- Main.java
+++ Main.java
@@ -1,1 +1,4 @@
 // Encapsulation means to hide the sensitve information
+// As we know the private specifier can be accessd within the class and not
+// access out side the class
+// To access from outside the class set and get methods are used

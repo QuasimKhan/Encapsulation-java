e9be3f464452c4608946227c4696eb8ca1cc8ccd
QuasimKhan <quasimkhanpersonal@gmail.com>
2023-10-16 20:36:58 +0530

    Updated

--- Main.java
+++ Main.java
@@ -2,3 +2,11 @@
 // As we know the private specifier can be accessd within the class and not
 // access out side the class
 // To access from outside the class set and get methods are used
+public class Main {
+    public static void main(String[] args) {
+        // The line `System.out.println("hello world");` is printing the string "hello
+        // world" to the
+        // console.
+        System.out.println("hello world");
+    }
+}
